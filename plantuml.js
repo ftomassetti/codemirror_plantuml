be6589db6d46ac5183b072232b4b6b8367a67930
Federico Tomassetti <federico@tomassetti.me>
2015-03-21 11:54:19 +0000

    Initial skeleton

---
+++ plantuml.js
@@ -0,0 +1,30 @@
+/*
+ * CodeMirror mode for PlantUML
+ * Written by Federico Tomassetti (http://tomassetti.me)
+ */
+
+(function(mod) {
+  if (typeof exports == "object" && typeof module == "object") // CommonJS
+    mod(require("CodeMirror/lib/codemirror"));
+  else if (typeof define == "function" && define.amd) // AMD
+    define(["CodeMirror/lib/codemirror"], mod);
+  else // Plain browser env
+    mod(CodeMirror);
+})(function(CodeMirror) {
+"use strict";
+
+CodeMirror.defineMode("PlantUML", function(config, parserConfig) {
+});
+
+CodeMirror.defineMIME("text/javascript", "javascript");
+CodeMirror.defineMIME("text/ecmascript", "javascript");
+CodeMirror.defineMIME("application/javascript", "javascript");
+CodeMirror.defineMIME("application/x-javascript", "javascript");
+CodeMirror.defineMIME("application/ecmascript", "javascript");
+CodeMirror.defineMIME("application/json", {name: "javascript", json: true});
+CodeMirror.defineMIME("application/x-json", {name: "javascript", json: true});
+CodeMirror.defineMIME("application/ld+json", {name: "javascript", jsonld: true});
+CodeMirror.defineMIME("text/typescript", { name: "javascript", typescript: true });
+CodeMirror.defineMIME("application/typescript", { name: "javascript", typescript: true });
+
+});
