252847c72f4a1d615e03a733295c7076030c8965
Federico Tomassetti <federico@tomassetti.me>
2015-03-28 11:09:16 +0000

    Tests for activity diagrams completed

--- test/plantuml_test.js
+++ test/plantuml_test.js
@@ -65,4 +65,55 @@
      "[keyword @enduml]"
   );  
 
+  MT("note_with_formatting",
+    "[keyword note] [keyword right]",
+    "[string This note is on several lines]",
+    "[string ....]",
+    "[string //This line is in italics//]",
+    "[string ----]",
+    "[string This one contains som <b>HTML</b>]",
+    "[string ====]",
+    "[string * This line contains a bullet]",
+    "[string ____]",
+    "[string \"\" This line is in code block\"\"]",
+    "[keyword end note]"
+  );
+
+  MT("repeat_block",
+    "[keyword repeat]",
+    "[def :Eat Hot Wings;]",
+    "[def :Drink Homebrew;]",
+    "[keyword repeat] [keyword while] [string (Still Hungry?)]"
+  );
+
+  MT("while_loop",
+    "[keyword while] [string (Hungry?)]  [keyword is] [string (Yes)]",
+    "    [def :Eat Hot Wings;]",
+    "    [def :Drink Homebrew;]",
+    "[keyword endwhile] [string (No)]"
+  );
+
+  MT("fork_join",
+    "[keyword if] [string (Turn On The Game?)] [keyword then] [string (yes)]",
+    "    [keyword fork]",
+    "        [def :__Having Fun__!!!;]",
+    "    [keyword fork] [keyword again]",
+    "        [def :Scream At TV!!;]",
+    "    [keyword end] [keyword fork]",
+    "[keyword else] [string (no)]",
+    "    [def :Not Having Fun;]",
+    "    [def :Eat Poptart;]",
+    "[keyword endif]"
+  );
+
+  MT("colors",
+    "[keyword skinparam] [keyword backgroundColor] [string #AAAAAA]",
+    "[keyword skinparam] [keyword activity] [operator {]",
+    "  [keyword StartColor] [string Blue]",
+    "  [keyword EndColor] [string Red]",
+    "  [keyword BackgroundColor] [string Green]",
+    "  [keyword BorderColor] [string Yellow]",
+    "[operator }]"
+  );
+
 })();
